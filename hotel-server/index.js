3cceb62bbee98666780a5925c75bb0a0af787576
synaptiatech <synaptia.tech@gmail.com>
2026-03-24 16:35:36 -0600

    Update index.js

--- hotel-server/index.js
+++ hotel-server/index.js
@@ -1,33 +1,31 @@
 import 'dotenv/config.js';
-// import './utils/env.utils.js';
 import app from './app.js';
 import { getOneQueryMethod } from './db/querys.js';
 import { __dirname, logger } from './utils/log.utils.js';
 import { VARS } from './config/vars.config.js';
 
 async function main() {
-	try {
-		// await sequelize.sync({ force: false });
-		// const response = await pool.query('SELECT NOW()');
+  try {
+    getOneQueryMethod({
+      table: 'now()',
+    })
+      .then((response) => {
+        console.log('Connected to the database:', response);
+      })
+      .catch((error) => {
+        logger(__dirname, 'main', error);
+      });
 
-		getOneQueryMethod({
-			table: 'now()',
-		})
-			.then((response) => {
-				console.log('Connected to the database:', response);
-			})
-			.catch((error) => {
-				logger(__dirname, 'main', error);
-			});
+    const PORT = process.env.PORT || VARS.API_PORT || 5005;
 
-		var PORT = VARS.API_PORT;
-		app.listen(PORT, () => {
-			console.info(`Server running on port ${PORT} 🚀`);
-			console.info({ VARS });
-		});
-	} catch (error) {
-		logger(__dirname, 'main', error);
-	}
+    app.listen(PORT, '0.0.0.0', () => {
+      console.info(`Server running on port ${PORT} 🚀`);
+      console.info({ VARS });
+    });
+  } catch (error) {
+    logger(__dirname, 'main', error);
+    process.exit(1);
+  }
 }
 
 main();
