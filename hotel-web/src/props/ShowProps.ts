270101ed194a7874f3b014389dbe4159b45c462c
synaptiatech <synaptia.tech@gmail.com>
2026-04-21 02:51:41 -0600

    Update ShowProps.ts

--- hotel-web/src/props/ShowProps.ts
+++ hotel-web/src/props/ShowProps.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import type { ReactNode } from 'react';
 
 export interface ItemList {
 	id: number;
@@ -21,7 +21,7 @@ export interface MiTablaProps {
 	onDelete?: (row: Object, index: number) => void;
 	onEdit?: (row: Object, index: number) => void;
 	onDownload?: (row: Object) => void;
-	cellRenderer?: { [key: string]: (value: any, row: any) => React.ReactNode };
+	cellRenderer?: { [key: string]: (value: any, row: any) => ReactNode };
 }
 
 export interface TableProps {
